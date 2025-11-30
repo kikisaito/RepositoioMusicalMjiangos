260ab9c1cc5f746d90965ffc8880372e0e978162
Jaitovich Bisnaud Jimeneqz <jaitovichbidnaud@gmail.com>
2025-11-30 14:29:16 -0600

    feat: implement audio controller and song info components

--- src/app/presentation/components/audio-controller/audio-controller.component.ts
+++ src/app/presentation/components/audio-controller/audio-controller.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlayerService } from '../../../core/services/player.service';
 
 @Component({
   selector: 'app-audio-controller',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './audio-controller.component.html',
   styleUrl: './audio-controller.component.scss'
 })
 export class AudioControllerComponent {
+  isPlaying$ = this.playerService.isPlaying$;
 
+  constructor(public playerService: PlayerService) { }
+
+  togglePlay(): void {
+    this.playerService.togglePlay();
+  }
 }
--- src/app/presentation/components/song-info/song-info.component.ts
+++ src/app/presentation/components/song-info/song-info.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlayerService } from '../../../core/services/player.service';
 
 @Component({
   selector: 'app-song-info',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './song-info.component.html',
   styleUrl: './song-info.component.scss'
 })
 export class SongInfoComponent {
+  currentTrack$ = this.playerService.currentTrack$;
 
+  constructor(public playerService: PlayerService) { }
 }
