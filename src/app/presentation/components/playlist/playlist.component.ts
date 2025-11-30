dd40b56c47054f85db563940513c9844a5a7a414
Jaitovich Bisnaud Jimeneqz <jaitovichbidnaud@gmail.com>
2025-11-30 14:40:01 -0600

    fix: resolve syntax errors in playlist component

--- src/app/presentation/components/playlist/playlist.component.ts
+++ src/app/presentation/components/playlist/playlist.component.ts
@@ -9,17 +9,6 @@ import { PlayerService } from '../../../core/services/player.service';
   templateUrl: './playlist.component.html',
   styleUrl: './playlist.component.scss'
 })
-  import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PlayerService } from '../../../core/services/player.service';
-
-@Component({
-  selector: 'app-playlist',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './playlist.component.html',
-  styleUrl: './playlist.component.scss'
-})
 export class PlaylistComponent {
   playlist$ = this.playerService.playlist$;
   currentTrack$ = this.playerService.currentTrack$;
